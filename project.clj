094294ba126f0c2c501bc16352e8534e902be4b0
Paul Iannazzo <somethingitalian@gmail.com>
2023-08-21 13:31:27 -0400

    Fix startup crashes caused by bad deps

--- project.clj
+++ project.clj
@@ -3,13 +3,16 @@
   :url "https://github.com/BrunoBonacci/ip-geoloc"
 
   :license {:name "Apache License 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :scm {:name "git" :url "https://github.com/BrunoBonacci/ip-geoloc"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.maxmind.geoip2/geoip2 "2.13.1"]
-                 [clj-http "3.10.0"]
-                 [pandect "0.6.1"]
-                 [com.brunobonacci/safely "0.5.0"]
-                 [org.clojure/tools.logging "1.0.0"]])
+  :dependencies [
+                 [clj-http/clj-http         "3.12.3"]
+                 [com.brunobonacci/safely   "0.5.0"]
+                 [com.maxmind.geoip2/geoip2 "4.1.0"]
+                 [org.clojure/clojure       "1.11.1"]
+                 [org.clojure/core.cache    "1.0.225"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [pandect/pandect           "1.0.2"]
+                 ])
